f8dcc25cfc47c62e554e940deef9e050d2689f68
kell-art <91781223+kell-art@users.noreply.github.com>
2021-10-08 16:44:52 +0300

    Add files via upload

---
+++ kelly 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,division;
+printf("\n enter value a:");
+scanf("%d",&a);
+printf("\n enter value b:");
+scanf("%d",&b);
+division=a/b;
+printf("n division=%d",division);
+return 0;
+}
