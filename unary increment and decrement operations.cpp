038a7b1afcbceb77aade302f41098faadebb0cd7
kell-art <91781223+kell-art@users.noreply.github.com>
2021-10-22 14:10:17 +0300

    Update unary increment and decrement operations.cpp

--- unary increment and decrement operations.cpp
+++ unary increment and decrement operations.cpp
@@ -1,8 +1,7 @@
 // C++ program to demonstrate working of unary increment
 // and decrement operators
 #include <iostream>
-using namespace std;
-  
+using namespace std;  
 int main()
 {
     // Post increment
@@ -10,28 +9,24 @@ int main()
     cout << "a value: " << a << endl;
     int b = a++;
     cout << "b value after a++ : " << b << endl;
-    cout << "a value after a++ : " << a << endl;
-  
+    cout << "a value after a++ : " << a << endl;  
     // Pre increment
     a = 1;
     cout << "a value:" << a << endl;
     b = ++a;
     cout << "b value after ++a : " << b << endl;
-    cout << "a value after ++a : "<< a << endl;
-  
+    cout << "a value after ++a : "<< a << endl;  
     // Post decrement
     a = 5;
     cout << "a value before decrement: " << a << endl;
     b = a--;
     cout << "b value after a-- : " << b << endl;
-    cout << "a value after a-- : " << a << endl;
-  
+    cout << "a value after a-- : " << a << endl;  
     // Pre decrement
     a = 5;
     cout << "a value: "<< a<<endl;
     b = --a;
     cout << "b value after --a : " << b << endl;
-    cout << "a value after --a : " << a << endl;
-  
+    cout << "a value after --a : " << a << endl;  
     return 0;
 }
