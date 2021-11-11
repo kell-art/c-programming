de3d9d45c72e9e350395dc1d62e42d8fdda8d12f
kell-art <91781223+kell-art@users.noreply.github.com>
2021-11-11 22:41:40 +0300

    Add files via upload

---
+++ While.cpp
@@ -0,0 +1,26 @@
+/*A programme to calculate area of a circle when radius is >=20*/ 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float radius;
+	float area;
+	while(radius>=20);
+	{
+		printf("Enter the radius of a circle\n");
+		scanf("%f",&radius);
+		area = M_PI * radius * radius;
+		printf("area is = %f",area);
+		{
+			if (area < 200);
+			{
+				printf("Less than normal");
+				{
+				if ("else");
+					printf("build the nation");
+				}
+			}
+		}
+	}
+	return 0;
+}
